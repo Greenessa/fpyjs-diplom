a3fca25452dcb44635e9bf471c31b29fbcc32083
Margo <romanova184@yandex.ru>
2025-01-20 18:26:46 +0300

    second commit

--- js/api/VK.js
+++ js/api/VK.js
@@ -1,10 +1,20 @@
+// const imgRequest = new XMLHttpRequest(); imgRequest.open('POST', 'https://api.vk.com/method/photos.get?owner_id=815147892&access_token=958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008&v=5.199&'); 
+// // imgRequest.responseType = "json"; 
+// imgRequest.setRequestHeader( 'Content-type', 'multipart/form-data' ); 
+// imgRequest.send(); imgRequest.upload.addEventListener('load', () =>  {let resp = imgRequest.response; console.log(resp);})
+
+// const imgRequest = new XMLHttpRequest(); imgRequest.open('POST', 'https://api.vk.com/method/photos.get?owner_id=815147892&access_token=958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008&v=5.199&callback=callbackFunc'); 
+// // imgRequest.responseType = "json"; 
+// imgRequest.setRequestHeader( 'Content-type', 'multipart/form-data' ); 
+// imgRequest.send(); imgRequest.upload.addEventListener('load', () =>  {let resp = imgRequest.response; console.log(resp);})
+
 /**
  * Класс VK
  * Управляет изображениями из VK. С помощью VK API.
  * С помощью этого класса будет выполняться загрузка изображений из vk.
  * Имеет свойства ACCESS_TOKEN и lastCallback
  * */
-export class VK {
+class VK {
 
   static ACCESS_TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008';
   static lastCallback;
@@ -14,11 +24,14 @@ export class VK {
    * */
   static get(id = '', callback){
     this.lastCallback = callback;
-    let scr = document.createElement('script');
-    src.classList.add('request');
-    let url = `https://api.vk.com/method/photos.get?owner_id=${id}&access_token=${this.ACCESS_TOKEN}&v=5.199`;
-    scr.innerText = `import {VK} from 'js/api/VK.js'; const imgRequestst = new XMLHttpRequest(); imgRequest.open('POST', ${url}); imgRequest.responseType = "json"; imgRequest.setRequestHeader( 'Content-type', 'multipart/form-data' ); imgRequest.send(); imgRequest.upload.addEventListener('load', () => VK.processData(imgRequest))`
-    document.body.insertAdjacentElement('beforeend', src);
+    let script = document.createElement('script');
+    script.classList.add('request');
+    script.src = `https://api.vk.com/method/photos.get?owner_id=${id}&access_token=${this.ACCESS_TOKEN}&v=5.199&callback=${VK.processData}`;
+    document.body.insertAdjacentElement('beforeend', script);
+    function callbackFunc(result) {
+      result.responseType = "json";
+      console.log(result.response)
+     }
   }
 
   /**
@@ -26,6 +39,8 @@ export class VK {
    * Является обработчиком ответа от сервера.
    */
   static processData(result){
+    // result.responseType = "json";
+    // console.log(result.response);
     let scriptReq = document.querySelector('.request');
     scriptReq.remove();
     result.addEventListener('load', (e) => {
@@ -39,4 +54,6 @@ export class VK {
     })
   }
 }
+
+
 
